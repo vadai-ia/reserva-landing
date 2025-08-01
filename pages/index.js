83d6ba1495d363d317ccef4c267281879b241323
VADAI <vadai.agencia.ai@gmail.com>
2025-08-01 13:23:32 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,8 +43,8 @@ export default function Home() {
 
   return (
     <div style={{
-      minHeight: '100vh',
-      background: '#fff',
+       minHeight: '100vh',
+      background: 'linear-gradient(135deg, #16213e 60%, #274382 90%, #4d7dfd 100%)',
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
